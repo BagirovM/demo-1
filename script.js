5a5cd2373bc585831d203cb99e5a99eb6ebb6e85
BagirovM <bagirovmurad224@gmail.com>
2022-07-28 16:28:32 +0400

    layihe

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
+const music = document.getElementById('audio')
+
+const progress = document.getElementById('progress')
+const progressContainer = document.getElementById('progress-container')
+const prevBtn =document.getElementById('prev')
+const playBtn =document.getElementById('play')
+const nextBtn =document.getElementById('next')
